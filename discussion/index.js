630a12db26e8be4c953bbf5329cfd3af8f81d00b
Michelle Pears Loquinario <mitchpearspel@gmail.com>
2021-12-14 19:59:41 +0800

    Done with do-while

--- discussion/index.js
+++ discussion/index.js
@@ -28,4 +28,42 @@ function countUsingWhile() {
 }
 }
 
-//
+//lets create a function that will count to a series of number depending on the value inserted by the user.
+
+function countUsingDoWhile() {
+	//get the input of the user.
+	let number = document.getElementById('task2').value;
+	//we have to make sure that the value is valid(we won't be accepting any values that are <= 0)
+	if (number <=0) {
+// the value is NOT VALID.
+// inform the user that he/she cannot proceed.
+// using DOM selectors we will target the container element.
+
+let displayText = document.getElementById('info');
+displayText.innerHTML = 'The number is Not Valid!';
+
+} else {
+// PROCEED because the value is VALID.
+alert(number);
+
+
+// syntax:
+// do {
+	// body of the loop
+		//} while condition
+		// task: will be to count from 1 to n (depending on the value inserted by the user.)
+		let indexStart = 1
+// the block of code indentified here will be executedfirst.
+let displayText = document.getElementById('info');
+
+displayText.innerHTML = number + ' is Valid';
+do {
+alert('count value: ' + indexStart);
+// or it can be console.log(indexStart); //for us to display the changes in the value of the indexStart variable. The do executes the code first. After it finishes the execution, the while statement will evaluate whether the do {...} will execute again at least once.
+
+indexStart++ //a value of 1 was added because the value was incremented.
+} while (indexStart<= number);
+
+}
+
+}
